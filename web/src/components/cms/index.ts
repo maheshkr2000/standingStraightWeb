e7c8ebdc5cd165405c2aebd4ed4b26b737c8e219
maheshkr2000 <86338932+maheshkr2000@users.noreply.github.com>
2025-12-18 23:34:32 +0530

    Update index.ts

--- web/src/components/cms/index.ts
+++ web/src/components/cms/index.ts
@@ -2,6 +2,7 @@ export { default as HeroSectionCMS } from './HeroSectionCMS';
 export { default as MissionSnapshotCMS } from './MissionSnapshotCMS';
 export { default as PatientStoriesPreviewCMS } from './PatientStoriesPreviewCMS';
 export { default as UpcomingEventsPreviewCMS } from './UpcomingEventsPreviewCMS';
+export { default as StoriesEventsCTA } from './StoriesEventsCTA';
 export { default as HowYouCanHelpCMS } from './HowYouCanHelpCMS';
 export { default as PartnersCMS } from './PartnersCMS';
 export { default as GlobalFootprintCMS } from './GlobalFootprintCMS';
